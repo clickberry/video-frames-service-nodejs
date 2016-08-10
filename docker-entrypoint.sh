05daff870886d8d14aa8702a978854a7b4fa3ab3
alexey-ernest <alexey.ernest@gmail.com>
2016-08-10 14:15:49 +0300

    A name of a folder for frames will be the same as file name without extension

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -n "$NSQLOOKUPD_PORT_4161_TCP_ADDR" ] && [ -n "$NSQLOOKUPD_PORT_4161_TCP_PORT" ]; then
-  export NSQLOOKUPD_ADDRESSES="http://${NSQLOOKUPD_PORT_4161_TCP_ADDR}:${NSQLOOKUPD_PORT_4161_TCP_PORT}"
-fi
-
 if [ -z "$NSQLOOKUPD_ADDRESSES" ]; then
     echo "NSQLOOKUPD_ADDRESSES environment variable required"
     exit 1
@@ -19,7 +15,6 @@ if [ -z "$S3_BUCKET" ]; then
     echo "S3_BUCKET environment variable required"
     exit 1
 fi
-
 
 echo "USING NSQLOOKUPD: ${NSQLOOKUPD_ADDRESSES}"
 
